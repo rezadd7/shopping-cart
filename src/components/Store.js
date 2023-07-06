a65fd1654fc14fd15a31a8a46a7ef5934fcf25ec
reza dehghan <rezadd7@gmail.com>
2023-07-06 19:44:26 +0330

    Create shopping cart and use context

--- src/components/Store.js
+++ src/components/Store.js
@@ -14,7 +14,7 @@ const Store = () => {
     const products = useContext(ProductsContext)
 
     return (
-        <div className=' flex flex-wrap justify-between Ise:justify-center IpA:justify-around mt-[150px]'>
+        <div className=' flex flex-wrap justify-between Ise:justify-center IpA:justify-around mt-[150px] mx-[120px] max-IpA:mx-[50px]'>
             {
                 products.map(item =><Product 
                                             key={item.id} 
